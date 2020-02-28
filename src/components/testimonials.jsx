d033640b11abaa4b8a1c7f15d3dac8e84a11968b
Panchu Sena <parshvshah5364@gmail.com>
2020-02-28 17:17:51 +0530

    Trial Designing

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -10,7 +10,7 @@ class Testimonials extends Component {
       <div className="jumbotron">
         {/* <h1>Hello World!</h1> */}
         <div className="row">
-          {this.state.feedbacks.map(feedback => (
+          {/* {this.state.feedbacks.map(feedback => (
             <div
               key={feedback._id}
               className="col-lg-4 border border-primary m-2"
@@ -21,18 +21,42 @@ class Testimonials extends Component {
               <h5>City : {feedback.UserInfo.City}</h5>
               <h5>State : {feedback.UserInfo.State}</h5>
             </div>
-          ))}
+          ))} */}
 
-          <div class="d-flex justify-content-start col-lg-2 border border-primary">
+          {/* For First Row  */}
+
+          <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
             ...
           </div>
-          <div class="d-flex justify-content-end col-lg-2 border border-primary">
-            ...
-          </div>
-          <div class="d-flex justify-content-center col-lg-2 border border-primary">
+          <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
             ...
           </div>
 
+          {/* For Middle Row */}
+
+          <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
+            ...
+          </div>
+
+          <div className="d-flex justify-content-center offset-lg-4"></div>
+
+          {/* For Last Row  */}
+
+          <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
+            ...
+          </div>
+
+          <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
+            ...
+          </div>
+
+          {/* Extra */}
+
+          {/* <div className="d-flex justify-content-center offset-lg-2 border border-primary"></div> */}
+          {/*
+          <div className="d-flex justify-content-end offset-lg-2 border border-primary">
+            ...
+          </div> */}
           {/* <div class="align-self-center col-lg-2 border border-primary">
             Aligned flex item
           </div> */}
--- src/services/feedback.js
+++ src/services/feedback.js
@@ -4,6 +4,45 @@ export const feedbacks = [
     Ratings: 4,
     FeedbackMsg: "This site is really awesome.",
     UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
+  },
+
+  {
+    Temp_Id: 1,
+    _id: "5e501444610567da80f7d1ea",
+    Ratings: 4,
+    FeedbackMsg: "This site is really awesome.",
+    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
+  },
+
+  {
+    Temp_Id: 2,
+    _id: "5e501444610567da80f7d1ea",
+    Ratings: 4,
+    FeedbackMsg: "This site is really awesome.",
+    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
+  },
+  {
+    Temp_Id: 3,
+    _id: "5e501444610567da80f7d1ea",
+    Ratings: 4,
+    FeedbackMsg: "This site is really awesome.",
+    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
+  },
+
+  {
+    Temp_Id: 4,
+    _id: "5e501444610567da80f7d1ea",
+    Ratings: 4,
+    FeedbackMsg: "This site is really awesome.",
+    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
+  },
+
+  {
+    Temp_Id: 5,
+    _id: "5e501444610567da80f7d1ea",
+    Ratings: 4,
+    FeedbackMsg: "This site is really awesome.",
+    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
   }
 ];
 
