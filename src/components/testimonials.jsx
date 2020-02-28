8eeff7f3d9051285362c61e6e5035c120cc1b056
Panchu Sena <parshvshah5364@gmail.com>
2020-02-29 01:00:59 +0530

    Added Testimonials

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -10,45 +10,47 @@ class Testimonials extends Component {
       <div className="jumbotron">
         {/* <h1>Hello World!</h1> */}
         <div className="row">
-          {/* {this.state.feedbacks.map(feedback => (
+          {this.state.feedbacks.map(feedback => (
             <div
-              key={feedback._id}
-              className="col-lg-4 border border-primary m-2"
+              key={feedback.Temp_Id}
+              className="col-lg-4 border justify-content-center border-primary m-2"
             >
-              <h2>Ratings : {feedback.Ratings}</h2>
-              <h2>Feedback Message : {feedback.FeedbackMsg}</h2>
-              <h5>UserName : {feedback.UserInfo.UserName}</h5>
-              <h5>City : {feedback.UserInfo.City}</h5>
-              <h5>State : {feedback.UserInfo.State}</h5>
+              <h1>
+                {feedback.UserInfo.UserName},
+                <span> {feedback.UserInfo.City}, </span>
+                <span>{feedback.UserInfo.State}</span>
+              </h1>
+              <h2>{feedback.FeedbackMsg}</h2>
+              <h2 className="justify-content-center">{feedback.Ratings}</h2>
             </div>
-          ))} */}
+          ))}
 
           {/* For First Row  */}
 
-          <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
+          {/* <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
             ...
           </div>
           <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
             ...
-          </div>
+          </div> */}
 
           {/* For Middle Row */}
 
-          <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
+          {/* <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
             ...
           </div>
 
-          <div className="d-flex justify-content-center offset-lg-4"></div>
+          <div className="d-flex justify-content-center offset-lg-4"></div> */}
 
           {/* For Last Row  */}
 
-          <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
+          {/* <div className="d-flex justify-content-center col-lg-4 border border-primary mb-2">
             ...
           </div>
 
           <div className="d-flex justify-content-center col-lg-4 offset-lg-4 border border-primary mb-2">
             ...
-          </div>
+          </div> */}
 
           {/* Extra */}
 
--- src/services/feedback.js
+++ src/services/feedback.js
@@ -1,12 +1,5 @@
 export const feedbacks = [
   {
-    _id: "5e501444610567da80f7d1ea",
-    Ratings: 4,
-    FeedbackMsg: "This site is really awesome.",
-    UserInfo: { UserName: "Ayush", City: "Patan", State: "Gujarat" }
-  },
-
-  {
     Temp_Id: 1,
     _id: "5e501444610567da80f7d1ea",
     Ratings: 4,
